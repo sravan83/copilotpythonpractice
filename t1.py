6d65b2dc0370316f5b53cbf16a80379f4b68e47c
sravan83 <154960236+sravan83@users.noreply.github.com>
2023-12-29 09:41:52 +0000

    com

--- t1.py
+++ t1.py
@@ -1,6 +1,17 @@
 # Task: Read the contents of 'data.txt' and print each line with line numbers
 
-with open('data.txt', 'r') as file:
-    # Use Copilot to complete the code to read and print each line with line numbers
-    for line in file:
-        # TODO: Complete the code here
+file_path = 'data.txt'
+
+try:
+    with open(file_path, 'r') as file:
+        # Read all lines from the file
+        lines = file.readlines()
+
+        # Print each line with line numbers
+        for line_number, line in enumerate(lines, start=1):
+            print(f"Line {line_number}: {line.strip()}")
+
+except FileNotFoundError:
+    print(f"Error: File '{file_path}' not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
